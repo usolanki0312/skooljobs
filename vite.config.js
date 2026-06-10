cc5ea62155427fab7765d35f4288d436180ad881
usolanki0312 <usolanki@cloudstrytech.com>
2026-06-10 14:10:16 +0530

    Basic PWA implimentation with vercel application for now

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,50 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    VitePWA({
+      injectRegister: "script",
+      registerType: "autoUpdate",
+      includeAssets: ["**/*.{js,css,html,ico,png,svg,jpg,jpeg,gif,webp}"],
+      manifest: {
+        name: "Skool Jobs",
+        short_name: "Skool Jobs",
+        description: "",
+        display: "standalone",
+        theme_color: "#575A59",
+        background_color: "#ffffff",
+        icons: [
+          {
+            src: "/logo192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/logo512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+        screenshots: [
+          {
+            src: "/screenshots/mobile-home.png",
+            sizes: "1080x1920",
+            type: "image/png",
+          },
+          {
+            src: "/screenshots/desktop-home.png",
+            sizes: "1920x1080",
+            type: "image/png",
+            form_factor: "wide",
+          },
+        ],
+      },
+    }),
   ],
-})
+});
